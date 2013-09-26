964e924544199c22708330bbefd8c95983a86b79
Nan Hartley <nan.hartley2002@gmail.com>
2013-09-26 18:39:13 -0400

    inital commit

---
+++ choose_ur_own_adventure.js
@@ -0,0 +1,25 @@
+confirm("I am so ready to play!");
+
+var userAge = prompt("What is your age?");
+
+if (userAge < 18)
+	}
+		console.log("You so should not be playing this game!  Go outside and play!");
+	{
+else 
+	{
+		console.log("Come into my lair and prepare yourself for battle!");
+	}
+
+	console.log("You are walking in a forest.  You come to a bog, where the foul stench of rotting human flesh fills your nostrils.");
+
+	var userChoiceBog = prompt("Will you 1 - go around the bog, or 2 - go through it?");
+
+	if (userChoiceBog == '1')
+	{
+		console.log("You have chosen to go around the bog.  Smart choice!  You live to fight another day!");
+	} 
+	else
+	{
+		console.log("You have chose to go through the bog.  Dumb choice!  You have died.  Please play again!");
+	}
